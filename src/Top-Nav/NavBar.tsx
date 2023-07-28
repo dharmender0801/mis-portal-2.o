d7e568ed0fdf6ec780b573ee0eed7bdbb0563aa4
dharmender0801 <teamsoul234@gmail.com>
2023-07-28 17:27:59 +0530

    Top Nav

---
+++ src/Top-Nav/NavBar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+const NavBar = (props: any) => {
+    return (
+        <>
+            <div className="nav">
+                <p>Voicechat</p>
+                <p>Voicechat</p>
+                <p>Voicechat</p>
+                <p>Voicechat</p>
+            </div>
+
+
+
+
+        </>
+    )
+}
+export default NavBar;
